5cedb8f0dc53444e5b23dfdb4fd00e5eadabb600
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2022-02-10 16:39:40 +0800

    isort

--- datumaro/plugins/kitti_format/extractor.py
+++ datumaro/plugins/kitti_format/extractor.py
@@ -7,7 +7,7 @@ import os.path as osp
 
 import numpy as np
 
-from datumaro.components.annotation import Bbox, LabelCategories, Mask, Cuboid3d
+from datumaro.components.annotation import Bbox, Cuboid3d, LabelCategories, Mask
 from datumaro.components.extractor import (
     AnnotationType, DatasetItem, SourceExtractor,
 )
