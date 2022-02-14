9940a9acb747501d85251095319ca79b934e7232
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2022-02-14 23:46:24 +0800

    Add 3D to kitti format

--- datumaro/plugins/kitti_format/extractor.py
+++ datumaro/plugins/kitti_format/extractor.py
@@ -7,7 +7,7 @@ import os.path as osp
 
 import numpy as np
 
-from datumaro.components.annotation import Bbox, LabelCategories, Mask
+from datumaro.components.annotation import Bbox, Cuboid3d, LabelCategories, Mask
 from datumaro.components.extractor import (
     AnnotationType, DatasetItem, SourceExtractor,
 )
@@ -20,10 +20,12 @@ from .format import (
 
 
 class _KittiExtractor(SourceExtractor):
-    def __init__(self, path, task, subset=None):
+    def __init__(self, path, task, subset=None, is_3d=False):
         assert osp.isdir(path), path
         self._path = path
         self._task = task
+
+        self._is_3d = is_3d
 
         if not subset:
             subset = osp.splitext(osp.basename(path))[0]
@@ -118,10 +120,19 @@ class _KittiExtractor(SourceExtractor):
                         label_id = self.categories()[AnnotationType.label].add(
                             line[0])
 
-                    anns.append(
-                        Bbox(x=x1, y=y1, w=x2-x1, h=y2-y1, id=line_idx,
-                            attributes=attributes, label=label_id,
+                    if self._is_3d:
+                        scale = [float(line[8]), float(line[9]), float(line[10])]
+                        position = [float(line[11]), float(line[12]), float(line[13])]
+                        rotation = [0, 0, float(line[14])]
+                        anns.append(Cuboid3d(
+                            position, rotation, scale, label=label_id,
+                            id=line_idx, attributes=attributes
                         ))
+                    else:
+                        anns.append(
+                            Bbox(x=x1, y=y1, w=x2-x1, h=y2-y1, id=line_idx,
+                                attributes=attributes, label=label_id,
+                            ))
 
                 items[item_id] = DatasetItem(id=item_id, annotations=anns,
                     image=image_path_by_id.pop(item_id, None),
@@ -138,9 +149,9 @@ class _KittiExtractor(SourceExtractor):
         return lambda: mask == c
 
 class KittiSegmentationExtractor(_KittiExtractor):
-    def __init__(self, path):
-        super().__init__(path, task=KittiTask.segmentation)
+    def __init__(self, path, **options):
+        super().__init__(path, task=KittiTask.segmentation, **options)
 
 class KittiDetectionExtractor(_KittiExtractor):
-    def __init__(self, path):
-        super().__init__(path, task=KittiTask.detection)
+    def __init__(self, path, **options):
+        super().__init__(path, task=KittiTask.detection, **options)
